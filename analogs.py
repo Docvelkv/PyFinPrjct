5c868492b5c28abea99dea867c4da8012d94f81a
Docvel <docvel@yandex.ru>
2023-03-25 01:39:19 +0300

    analog of the method get_dummies has been created

---
+++ analogs.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+
+def analog_get_dummies(ser_data: pd.Series) -> pd.DataFrame:
+    """
+    Упрощенный аналог метода get_dummies.
+    Конвертирует категориальные значения в числовые.
+
+    Args:
+        ser_data (pd.Series): Объект Series из DataFrame.
+
+    Returns:
+        pd.DataFrame: Объект DataFrame
+    """
+    add_name = ser_data.name
+    lst_val = ser_data.tolist()
+    uniq_val = ser_data.unique().tolist()
+    new_dict = {}
+    for ik in range(len(uniq_val)):
+        new_lst = []
+        for jv in range(len(lst_val)):
+            if uniq_val[ik] == lst_val[jv]:
+                new_lst.append(1)
+            else:
+                new_lst.append(0)
+        new_dict[f"{add_name}_{uniq_val[ik]}"] = new_lst
+    return pd.DataFrame(new_dict)
